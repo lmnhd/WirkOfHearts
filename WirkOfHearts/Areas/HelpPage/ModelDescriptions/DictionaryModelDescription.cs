48bf576b29d433af2f12c9d19d7a35b701692d46
lmnhd <cc.lemonhead@gmail.com>
2015-03-01 23:05:26 -0500

    Initial repo

---
+++ WirkOfHearts/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WirkOfHearts.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WirkOfHearts/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WirkOfHearts.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
